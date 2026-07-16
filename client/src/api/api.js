1ab8b278b4a6304020c8ebc29810d70e8349ae41
Nandinisahu75 <nandini9232@gmail.com>
2026-07-16 10:41:50 +0530

    Updated API URL for production deployment

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://taskflow-api-e833.onrender.com/api",
 });
 
 export default API;
